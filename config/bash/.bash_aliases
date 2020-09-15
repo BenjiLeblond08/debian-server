6cdac3a303b8be5e6de3e85c55b7144a8e275f66
Julia Leblond <julia.lblnd@free.fr>
2020-09-16 00:39:50 +0200

    changing the way i add my config

---
+++ config/bash/.bash_aliases
@@ -0,0 +1,20 @@
+alias dir='dir --color=auto'
+alias vdir='vdir --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+
+alias ls='ls -h --color=auto'
+alias zshconfig="mate ~/.zshrc"
+alias ohmyzsh="mate ~/.oh-my-zsh"
+alias bye="cd && exit && exit"
+alias back='cd $OLDPWD'
+alias e.="explorer.exe ."
+alias tracert='tracert.exe'
+alias subl="/mnt/c/Program\ Files/Sublime\ Text\ 3/sublime_text.exe"
+alias st="subl"
+alias go='go.exe'
---
+++ config/oh-my-zsh/.oh-my-zsh/custom/julialblnd.zsh
@@ -0,0 +1,41 @@
+export PATH=$HOME/bin:/usr/local/bin:$PATH
+ZSH_THEME="nebjix"
+plugins=(git)
+
+# User configuration
+
+export MANPATH="/usr/local/man:$MANPATH"
+
+# You may need to manually set your language environment
+export LANG=en_US.UTF-8
+
+# Preferred editor for local and remote sessions
+if [[ -n $SSH_CONNECTION ]]; then
+  export EDITOR=vim
+else
+  export EDITOR=vim
+fi
+
+# Compilation flags
+# export ARCHFLAGS="-arch x86_64"
+
+if [[ "$(pwd)" == "/mnt/c/Windows/System32" ]]; then
+	cd ${HOME}
+	clear
+fi
+
+# Set personal aliases, overriding those provided by oh-my-zsh libs,
+# plugins, and themes. Aliases can be placed here, though oh-my-zsh
+# users are encouraged to define aliases within the ZSH_CUSTOM folder.
+# For a full list of active aliases, run `alias`.
+
+alias ls='ls -h --color=auto'
+alias zshconfig="mate ~/.zshrc"
+alias ohmyzsh="mate ~/.oh-my-zsh"
+alias bye="cd && exit && exit"
+alias back='cd $OLDPWD'
+alias e.="explorer.exe ."
+alias tracert='tracert.exe'
+alias subl="/mnt/c/Program\ Files/Sublime\ Text\ 3/sublime_text.exe"
+alias st="subl"
+alias go='go.exe'
